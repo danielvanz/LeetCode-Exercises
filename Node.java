4ba98eaf3ed5bb59273b7de7abb2427675b1f61c
Daniël <d.a.v.ziel@student.tue.nl>
2022-11-14 20:11:32 +0100

    First Commit

--- Node.java
+++
@@ -1,8 +0,0 @@
-public class Node {
-    int val;
-    Node next;
-
-    public Node(int val) {
-        this.val = val;
-    }
-}
--- practice.js
+++
@@ -1,6 +0,0 @@
-function capitalize(str) {
-    let firstLetter = str[0]
-    let remaining = str.slice(1)
-    firstLetter.capitalize
-    return firstLetter + remaining
-}
